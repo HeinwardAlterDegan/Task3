afcaf4d3e6bfc604b68c14f145e57c9b6b187a22
Эдуард Гусев <f1ful1337@gmail.com>
2024-10-07 00:17:32 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Ввведите трёхзначное число N:");
+            int N= int.Parse(Console.ReadLine());
+            int units = N/100;
+            int tens = N/10%10*10;
+            int hundreds = N % 10 * 100;
+            int X = units+tens+hundreds;
+            Console.WriteLine("Искомое число X:" + X);
+        Console.ReadKey();
+        }
+    }
+}
